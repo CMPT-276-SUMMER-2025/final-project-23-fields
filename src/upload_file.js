54e447e7f943f61e49dc6ca6fa2e0922a088394e
Ennil23541 <Cherrypcchan@gmail.com>
2025-07-21 16:06:28 -0700

    Changed button to button and input to upload a single pdf file, linked Javascript to application html, pdf uploading test is successfully outputed to console log

--- src/upload_file.js
+++ src/upload_file.js
@@ -1,1 +1,7 @@
 //This is for the pdf upload functions
+let uploaded_file="";
+
+function show_uploaded_file(){
+    uploaded_file = document.querySelector('.uploaded_pdf');
+    console.log(uploaded_file.value);
+}

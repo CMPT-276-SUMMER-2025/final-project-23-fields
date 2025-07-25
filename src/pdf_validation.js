e2b088f884006856c3dac6015b7f004671ebc543
Ennil23541 <Cherrypcchan@gmail.com>
2025-07-25 00:08:20 -0700

    Included pdf.js as API/JS library/stack, configuration successful and build a simple pdf file validation function on pdf_validation.js, removed update_file_name and file name will be accessed with uploaded_file.files[0].name, lastly I added another varible is_pdf to store the result of validation, next is to remove redundant code and comments, fix the is_pdf logic in upload_file.js and write in the readme.md to let people know how to modify the pdf.js functions

--- src/pdf_validation.js
+++ src/pdf_validation.js
@@ -1,5 +1,47 @@
-function vaildation(){
-    const pdf_url = uploaded_file.value;
-    if(pdf_url !== ""){
-    console.log(pdf_url);}
+function validation(){
+    const pdf_url = `${uploaded_file.value}`;
+    if(pdf_url !== "" && uploaded_file.files[0].name.split('.')[1] === "pdf"){
+        is_pdf = true;
+        console.log(pdf_url);
+        
+
+
+        // Assuming 'pdfFile' is a File object obtained from an <input type="file"> or drag-and-drop
+        const reader = new FileReader();
+
+        // Event listener for when the file has been successfully loaded
+        reader.onload = function(event) {
+                let typedarray = new Uint8Array(this.result);
+
+        //Step 5:pdfjs should be able to read this
+        const loadingTask = pdfjsLib.getDocument(typedarray);
+        loadingTask.promise.then(pdf => {
+            console.log(pdf.numPages);
+        });
+        // You can now process 'pdfContent' (e.g., pass it to a PDF rendering library)
+        //console.log("PDF content loaded:", pdfContent);
+        };
+        reader.readAsArrayBuffer(uploaded_file.files[0]);
+
+
+
+
+
+        // Event listener for errors during the read operation
+        reader.onerror = function(event) {
+        console.error("Error reading PDF file:", event.target.error);
+        };
+
+        // Alternatively, read the PDF file as a DataURL for base64 encoded data
+        // reader.readAsDataURL(pdfFile);  
+    }
+    else{
+        is_pdf=false;
+        console.log("invalid upload file");
+    }
+    console.log(uploaded_file.files[0].name.split('.')[1]);
 }
+
+
+
+
--- src/upload_file.js
+++ src/upload_file.js
@@ -1,23 +1,25 @@
 
 
-function update_file_name(event){
+/*function update_file_name(event){
     console.log(event);
     const files = event.target.name;
-    file_name=files[0].name;
-}
+    file_name=files[0].name;onchange="addEventListener('change',update_file_name);
+}*/
 
 function show_uploaded_file(){
     uploaded_file = document.querySelector('.uploaded_pdf');
+    validation();
+    if (is_pdf){
     //display pdf icon, name and remove button
     if(uploaded_file.value !== ""){
         have_file = true;
         //locate div and change content
         const pdf_icon_output = document.querySelector('.upload_document');
         pdf_icon_output.innerHTML = `<button style="height: fit-content; width: fit-content;" onclick="document.querySelector('.uploaded_pdf').click();">Upload file</button>
-            <input type="file" class="uploaded_pdf" accept=".pdf" style="display: none;" onchange="show_uploaded_file();">
+            <input type="file" class="uploaded_pdf" accept=".pdf" style="display: none;" onchange="show_uploaded_file(); ">
             <div class="pdf_item">
                 <div>
-                    <button class="pdf_remove_button" onclick="havefile(); remove_pdf_file();">
+                    <button class="pdf_remove_button" onclick="remove_pdf_file();">
                         <img src="icons/remove_icon.png" style="height: 25px; width: 25px; object-fit: contain; margin: 0px;">
                     </button>
                     <img class="pdf_icon" src="icons/pdf_logo.png">
@@ -26,10 +28,11 @@ function show_uploaded_file(){
             </div>`;
     }
 }
+}
 
 function remove_pdf_file(){
     have_file = false;
     const pdf_icon_output = document.querySelector('.upload_document');
     pdf_icon_output.innerHTML = `<button style="height: fit-content; width: fit-content;" onclick="document.querySelector('.uploaded_pdf').click();">Upload file</button>
-            <input type="file" class="uploaded_pdf" accept=".pdf" style="display: none;" onchange="addEventListener('change',update_file_name); show_uploaded_file();" >`;
+            <input type="file" class="uploaded_pdf" accept=".pdf" style="display: none;"  onchange="show_uploaded_file();" >`;
 }

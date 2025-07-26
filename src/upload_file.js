64573d3a0009bc7f27c19f95375e6eee51ddfaa0
Simon Lee <infosimonlee@yahoo.com>
2025-07-26 02:27:41 -0700

    Added PDF Parser and fixed bugs in html button JS functions

--- src/upload_file.js
+++ src/upload_file.js
@@ -2,21 +2,23 @@
 let uploaded_file="";
 let have_file = false;
 let file_name="";
+pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.4.120/pdf.worker.min.js';
 
 function update_file_name(event){
     console.log(event);
-    const files = event.target.name;
+    const files = event.target.files;
     file_name=files[0].name;
+    uploaded_file = files[0];
 }
 
 function show_uploaded_file(){
-    uploaded_file = document.querySelector('.uploaded_pdf');
+   
     //display pdf icon, name and remove button
-    if(uploaded_file.value !== ""){
+    if(uploaded_file.valueOf !== ""){
         have_file = true;
         //locate div and change content
         const pdf_icon_output = document.querySelector('.upload_document');
-        pdf_icon_output.innerHTML = `<button style="height: fit-content; width: fit-content;" onclick="document.querySelector('.uploaded_pdf').click();">Upload file</button>
+        pdf_icon_output.innerHTML = `<button style="height: fit-content; width: fit-content;" id="uploadbtn" onclick="document.querySelector('.uploaded_pdf').click();">Upload file</button>
             <input type="file" class="uploaded_pdf" accept=".pdf" style="display: none;" onchange="show_uploaded_file();">
         <!-- Below is for Javascript-->
             <div class="pdf_item">
@@ -26,14 +28,42 @@ function show_uploaded_file(){
                     </button>
                     <img class="pdf_icon" src="icons/pdf_logo.png">
                 </div>
-                <p style="margin-left: 25px; font-family: Arial; font-size: 16px; font-weight: 600;">${uploaded_file.files[0].name}</p>
+                <p style="margin-left: 25px; font-family: Arial; font-size: 16px; font-weight: 600;">${file_name}</p>
             </div>`;
+        document.getElementById("uploadbtn").style.display = "none"
     }
 }
 
 function remove_pdf_file(){
     have_file = false;
     const pdf_icon_output = document.querySelector('.upload_document');
-    pdf_icon_output.innerHTML = `<button style="height: fit-content; width: fit-content;" onclick="document.querySelector('.uploaded_pdf').click();">Upload file</button>
-            <input type="file" class="uploaded_pdf" accept=".pdf" style="display: none;" onchange="addEventListener('change',update_file_name); show_uploaded_file();" >`;
+    pdf_icon_output.innerHTML = `<button style="height: fit-content; width: fit-content;" id="uploadbtn" onclick="document.querySelector('.uploaded_pdf').click();">Upload file</button>
+            <input type="file" class="uploaded_pdf" accept=".pdf" style="display: none;" onchange="update_file_name(event); show_uploaded_file();" >`;
+            document.getElementById("uploadbtn").style.display = "block"
+}
+
+async function processPDF() {
+    if (uploaded_file == "") {
+        alert("Please upload a file")
+        return;
+    }
+     const reader = new FileReader();
+      reader.onload = async function () {
+        console.log("working")
+        const typedArray = new Uint8Array(reader.result);
+
+        const pdf = await pdfjsLib.getDocument({ data: typedArray }).promise;
+
+        let fullText = '';
+        for (let pageNum = 1; pageNum <= pdf.numPages; pageNum++) {
+          const page = await pdf.getPage(pageNum);
+          const content = await page.getTextContent();
+          const text = content.items.map(item => item.str).join(' ');
+          fullText += text + '\n';
+        }
+    
+
+        console.log("Extracted Text:", fullText);
+    }
+    reader.readAsArrayBuffer(uploaded_file)
 }

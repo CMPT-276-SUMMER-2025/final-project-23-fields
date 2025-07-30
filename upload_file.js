6e4327ebb8fc15abe11af1885f3cea76091c8dbe
Simon Lee <infosimonlee@yahoo.com>
2025-07-30 12:05:06 -0700

    Added latest changes to root

--- upload_file.js
+++ upload_file.js
@@ -1,21 +1,25 @@
 async function show_uploaded_file(){
     uploaded_file = document.querySelector('.uploaded_pdf'); 
     console.log(have_file, is_pdf, pdf_too_large);
-    validation();   
-    /*const validation_promise = new Promise((resolve,reject) => {validation();
-        if(!pdf_too_large){
-            resolve("validation success");
-        }
-        else{
-            reject("file too big");
-        }
+    async function update_varibles() {
+                    validation(pdf_json_input);        
+                    await sleep(7500);
 
-    });*/
-    
+                }
+    update_varibles();    
 
+    //pdf_json_input = await validation();
+    console.log(pdf_json_input);
+    await update_varibles();
+    console.log(pdf_json_input);
+    if(pdf_json_input.content !== ""){
+        pdf_json_input.name = "text transaction record";
+        transaction_json = JSON.stringify(pdf_json_input);
+    }
     //console.log(have_file, is_pdf, pdf_too_large);
     await replace();
 }
+
 
 async function replace() {
     /*const result = await validation();
@@ -23,6 +27,12 @@ async function replace() {
         console.log(have_file, is_pdf, pdf_too_large);
         console.log(pdf_too_large);
         if (uploaded_file.value !== "" && is_pdf && have_file && !pdf_too_large){//display pdf icon, name and remove button if the pdf meet all requirements
+            if(invalid_file){
+                const pdf_icon_output = document.querySelector('.upload_statement_box_invalid_file');
+                pdf_icon_output.classList.add('upload_statement_box');
+                pdf_icon_output.classList.remove('upload_statement_box_invalid_file');
+                invalid_file = false;
+            }
         //locate div and change content
         const pdf_icon_output = document.querySelector('.upload_document');
         pdf_icon_output.innerHTML = `<button style="height: fit-content; width: fit-content;" onclick="document.querySelector('.uploaded_pdf').click();">Upload file</button>
@@ -37,7 +47,92 @@ async function replace() {
                 <p style="margin-left: 25px; font-family: Arial; font-size: 16px; font-weight: 600;">${uploaded_file.files[0].name}</p>
             </div>`;
     
-    }
+        }
+        else if(uploaded_file.value === ""){
+            if(!invalid_file){
+                const pdf_icon_output = document.querySelector('.upload_statement_box');
+                pdf_icon_output.classList.add('upload_statement_box_invalid_file');
+                pdf_icon_output.classList.remove('upload_statement_box');
+                invalid_file = true;
+            }
+            const pdf_icon_output = document.querySelector('.upload_statement_box_invalid_file');
+            pdf_icon_output.innerHTML = `<div class="box_title">
+            <div>
+                <img src="icons/upload.png" style="height: 30px; width: 30px; object-fit: scale-down;">
+            </div>
+            <p class="title_text">Upload Statement</p>
+            </div>
+
+            <div class="upload_document">
+            
+            
+            <button style="height: fit-content; width: fit-content; background-color: orange;" onclick="document.querySelector('.uploaded_pdf').click();">Upload file</button>
+            <input type="file" class="uploaded_pdf" accept=".pdf" style="display: none;" onchange="show_uploaded_file();" >
+            </div>
+
+            <div class="pdf_error_message">
+            <p style="margin-top: 5px; margin-bottom: 5px;">Oops, it seems you haven't uploaded anything, please try again</p>
+            </div>
+
+            <button class="div_button" Onclick="console.log(pdf_json_input.content)">Extract Transactions</button>`;
+        }
+        else if(!is_pdf){
+            if(!invalid_file){
+                const pdf_icon_output = document.querySelector('.upload_statement_box');
+                pdf_icon_output.classList.add('upload_statement_box_invalid_file');
+                pdf_icon_output.classList.remove('upload_statement_box');
+                invalid_file = true;
+            }
+            const pdf_icon_output = document.querySelector('.upload_statement_box_invalid_file');
+            console.log(pdf_icon_output);
+            pdf_icon_output.innerHTML = `<div class="box_title">
+            <div>
+                <img src="icons/upload.png" style="height: 30px; width: 30px; object-fit: scale-down;">
+            </div>
+            <p class="title_text">Upload Statement</p>
+            </div>
+
+            <div class="upload_document">
+            
+            
+            <button style="height: fit-content; width: fit-content; background-color: orange;" onclick="document.querySelector('.uploaded_pdf').click();">Upload file</button>
+            <input type="file" class="uploaded_pdf" accept=".pdf" style="display: none;" onchange="show_uploaded_file();" >
+            </div>
+
+            <div class="pdf_error_message">
+            <p style="margin-top: 5px; margin-bottom: 5px;">Please sumbit a file that is ended in .pdf</p>
+            </div>
+
+            <button class="div_button" Onclick="console.log(pdf_json_input.content)">Extract Transactions</button>`;
+        }
+        else if(pdf_too_large){
+            if(!invalid_file){
+                const pdf_icon_output = document.querySelector('.upload_statement_box');
+                pdf_icon_output.classList.add('upload_statement_box_invalid_file');
+                pdf_icon_output.classList.remove('upload_statement_box');
+                invalid_file = true;
+            }
+            const pdf_icon_output = document.querySelector('.upload_statement_box_invalid_file');
+            pdf_icon_output.innerHTML = `<div class="box_title">
+            <div>
+                <img src="icons/upload.png" style="height: 30px; width: 30px; object-fit: scale-down;">
+            </div>
+            <p class="title_text">Upload Statement</p>
+            </div>
+
+            <div class="upload_document">
+            
+            
+            <button style="height: fit-content; width: fit-content; background-color: orange;" onclick="document.querySelector('.uploaded_pdf').click();">Upload file</button>
+            <input type="file" class="uploaded_pdf" accept=".pdf" style="display: none;" onchange="show_uploaded_file();" >
+            </div>
+
+            <div class="pdf_error_message">
+            <p style="margin-top: 5px; margin-bottom: 5px;">Please submit a file that is smaller than 10MB</p>
+            </div>
+
+            <button class="div_button" Onclick="console.log(pdf_json_input.content)">Extract Transactions</button>`;
+        }
     }
 
 function remove_pdf_file(){
@@ -70,7 +165,7 @@ async function processPDF() {
         messages: [
           {
             role: "user",
-            content: "Write just a summary of how to budget myself based on this data:"
+            content: "Say Hello to everyone and tell them this area is under construction"
           }
         ]
       })
